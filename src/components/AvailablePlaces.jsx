faffbb625ea756f9d53e22b3117bcf80c80de733
user.email <jesus.macedo.michelada@kueski.com>
2024-10-15 15:53:18 -0600

    Creating custom hooks

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
+
+// navigator.geolocation.getCurrentPosition((position) => {
+//   const sortedPlaces = sortPlacesByDistance(
+//     places,
+//     position.coords.latitude,
+//     position.coords.longitude
+//   );
+//   setAvailablePlaces(sortedPlaces);
+//   setIsFetching(false);
+// });
+export default function AvailablePlaces({ onSelectPlace }) {
+  const { isFetching, error, fetchedData: availablePlaces, 
+    setFetchedData: setAvailablePlaces } = useFetch(fetchAvailablePlaces, [])
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
